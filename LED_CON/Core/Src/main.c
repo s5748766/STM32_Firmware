24e03a5933b1387d92185e659644c4db7dd36313
s5748766 <s5748766@gmail.com>
2025-09-12 11:00:31 +0900

    first_commit

---
+++ LED_CON/Core/Src/main.c
@@ -0,0 +1,487 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+int stack[10];
+uint8_t transmit[256];
+uint8_t receive;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+	if(ch == '\n')
+		HAL_UART_Transmit(&huart2, (uint8_t *)"\r", 1, 0xFFFF);
+		HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+
+		return ch;
+}
+
+void smartcar_F(void)
+{
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 1);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 1);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 1);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 1);
+	HAL_Delay(500);
+}
+
+void smartcar_B(void)
+{
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 1);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 1);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 1);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 1);
+	HAL_Delay(500);
+}
+
+void smartcar_S(void)
+{
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+	HAL_Delay(500);
+}
+
+void smartcar_FL(void)
+{
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 1);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 1);
+	HAL_Delay(500);
+}
+
+
+void smartcar_FR(void)
+{
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 1);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 1);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+	HAL_Delay(500);
+}
+
+void smartcar_BL(void)
+{
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 1);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 1);
+	HAL_Delay(500);
+}
+
+
+void smartcar_BR(void)
+{
+	HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+	HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+	HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+	HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 1);
+	HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 1);
+	HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+	HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+	HAL_Delay(500);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  printf("=== RC CAR Test ===\r\n");
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  printf("=== Enter your word ===\r\n");
+	  HAL_UART_Receive(&huart2, &receive, 1, HAL_MAX_DELAY);
+
+	  if(receive == 'W')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "GO\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_F();
+	  }
+
+	  if(receive == 'S')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "BACK\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_B();
+	  }
+
+	  if(receive == 'A')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "F_LEFT\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_FL();
+	  }
+
+	  if(receive == 'D')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "F_RIGHT\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_FR();
+	  }
+
+	  if(receive == 'Z')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "B_LEFT\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_BL();
+	  }
+
+	  if(receive == 'C')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 1);
+		  sprintf((char *)transmit, "B_RIGHT\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_BR();
+	  }
+
+	  if(receive == 'P')
+	  {
+		  printf("\r\n");
+		  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, 0);
+		  sprintf((char *)transmit, "OFF\n\r");
+		  HAL_UART_Transmit(&huart2, &transmit, strlen((char *)transmit), 100);
+		  smartcar_S();
+	  }
+
+
+
+//	  HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LFF_GPIO_Port, LFF_Pin, 0);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LFB_GPIO_Port, LFB_Pin, 0);
+//	  HAL_Delay(500);
+//
+//	  HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LBF_GPIO_Port, LBF_Pin, 0);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(LBB_GPIO_Port, LBB_Pin, 0);
+//	  HAL_Delay(500);
+//
+//	  HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RFF_GPIO_Port, RFF_Pin, 0);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RFB_GPIO_Port, RFB_Pin, 0);
+//	  HAL_Delay(500);
+//
+//	  HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RBF_GPIO_Port, RBF_Pin, 0);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 1);
+//	  HAL_Delay(500);
+//	  HAL_GPIO_WritePin(RBB_GPIO_Port, RBB_Pin, 0);
+//	  HAL_Delay(500);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED2_Pin|LD2_Pin|LBF_Pin|LFB_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LBB_Pin|LFF_Pin|RFF_Pin|RFB_Pin
+                          |RBF_Pin|RBB_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED2_Pin LD2_Pin LBF_Pin LFB_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin|LD2_Pin|LBF_Pin|LFB_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LBB_Pin LFF_Pin RFF_Pin RFB_Pin
+                           RBF_Pin RBB_Pin */
+  GPIO_InitStruct.Pin = LBB_Pin|LFF_Pin|RFF_Pin|RFB_Pin
+                          |RBF_Pin|RBB_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
