2b31ae91faaa9571e3daee58bfbdcae752a04e67
s5748766 <s5748766@gmail.com>
2025-09-19 14:47:02 +0900

    uart2 printf.h, .c update

---
+++ uart_com/Core/Inc/uart2_printf.h
@@ -0,0 +1,14 @@
+/*
+ * uart2_printf.h
+ *
+ * Created on: 2025. 03. 16 by Lee Yongjin
+ *
+ * STM32 HAL library for using printf with USART2
+ */
+  
+#ifndef UART2_PRINTF_H
+#define UART2_PRINTF_H
+
+#include "stm32f1xx_hal.h"
+#include<stdio.h>
+#endif /* UART2_PRINTF_H */
---
+++ uart_com/Core/Src/uart2_printf.c
@@ -0,0 +1,35 @@
+/*
+ * uart2_printf.c
+ *
+ * Created on: 2025. 03. 16 by Lee Yongjin
+ *
+ * STM32 HAL library for using printf with USART2
+ */
+
+#include "uart2_printf.h"
+
+extern UART_HandleTypeDef huart2;
+
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  if (ch == '\n')
+    HAL_UART_Transmit (&huart2, (uint8_t*) "\r", 1, 0xFFFF);
+  HAL_UART_Transmit (&huart2, (uint8_t*) &ch, 1, 0xFFFF);
+
+  return ch;
+}
